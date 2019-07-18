cb350e97524cc2017e3c7622f8b69ef4ebe21f67
Janche <957671816@qq.com>
2019-07-18 09:40:01 +0800

    更改 清除session的方式

--- security/src/main/java/com/example/janche/security/hadler/SecurityLogoutSuccessHandler.java
+++ security/src/main/java/com/example/janche/security/hadler/SecurityLogoutSuccessHandler.java
@@ -1,9 +1,9 @@
 package com.example.janche.security.hadler;
 
 import com.example.janche.common.config.IApplicationConfig;
-import com.example.janche.common.util.IPUtils;
 import com.example.janche.common.restResult.RestResult;
 import com.example.janche.common.restResult.ResultCode;
+import com.example.janche.common.util.IPUtils;
 import com.example.janche.log.domain.SysLog;
 import com.example.janche.log.service.SysLogService;
 import com.example.janche.security.utils.ResponseUtils;
@@ -15,10 +15,8 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -33,11 +31,10 @@ public class SecurityLogoutSuccessHandler implements LogoutSuccessHandler {
 	private SysLogService sysLogService;
 
 	@Override
-	public void onLogoutSuccess(HttpServletRequest request , HttpServletResponse response , Authentication authentication) throws IOException,
-            ServletException {
+	public void onLogoutSuccess(HttpServletRequest request , HttpServletResponse response , Authentication authentication) throws IOException {
 
 		// 清除登录的session
-		this.removeSesion(request);
+		request.getSession().invalidate();
 
         // 记录登出日志
         if (null != authentication) {
@@ -52,16 +49,6 @@ public class SecurityLogoutSuccessHandler implements LogoutSuccessHandler {
 		PrintWriter writer = response.getWriter();
 		writer.print(result.toJson());
 		writer.flush();
-	}
-
-	/**
-	 * 移除登录用户的session
-	 * @param request
-	 */
-	private void removeSesion(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		// 登出后1秒，手动让系统中的此session失效
-		session.setMaxInactiveInterval(1);
 	}
 	
 	/**
