c7f626af31ec4a27bd50fb9f12a928db8d9f8569
Janche <957671816@qq.com>
2020-09-16 10:42:26 +0800

    :rocket: 优化swagger接文档，界面采用knife4j风格

--- swagger/src/main/java/com/example/janche/swagger/SwaggerConfig.java
+++ swagger/src/main/java/com/example/janche/swagger/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.example.janche.swagger;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,11 +17,13 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 @EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfig {
     @Autowired
     private SwaggerProperties swaggerProperties;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -32,15 +37,15 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-        if(swaggerProperties.getTitle()!=null){
+        if (swaggerProperties.getTitle() != null) {
             apiInfoBuilder.title(swaggerProperties.getTitle());
         }
-        if(swaggerProperties.getContractName()!=null&&swaggerProperties.getContractUrl()!=null&&
-                swaggerProperties.getContractEmail()!=null){
+        if (swaggerProperties.getContractName() != null && swaggerProperties.getContractUrl() != null
+                && swaggerProperties.getContractEmail() != null) {
             apiInfoBuilder.contact(new Contact(swaggerProperties.getContractName(), swaggerProperties.getContractUrl(),
                     swaggerProperties.getContractEmail()));
         }
-        if(swaggerProperties.getVersion()!=null){
+        if (swaggerProperties.getVersion() != null) {
             apiInfoBuilder.version(swaggerProperties.getVersion());
         }
         return apiInfoBuilder.build();
